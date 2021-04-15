4264aa452f160370b78a682cc5d3e9abe54119f4
Ergün Efe Turanlı <ergunefeturanli@gmail.com>
2021-04-15 23:11:13 +0300

    fix minor bug

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -3,7 +3,7 @@ const request = require('request')
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiZXJndW5lZmV0dXJhbmxpIiwiYSI6ImNrbjR3bThyazFzN3EycnF1cGdoY3FxM3gifQ.xE-zal5P5Od3iWMVlaJMAg&limit=1'
 
-    request({ url, json: true }, (error, { body }) => {
+    request({ url, json: true }, (error, { body }={}) => {
         if (error) {
             callback('Unable to connect to location services!', undefined)
         } else if (body.message || body.features.length === 0) {
